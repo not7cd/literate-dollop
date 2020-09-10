a67a9575e48ec5589d432a09a1cbd880ee063d36
Norbert Szulc <norbert@not7cd.net>
2020-09-11 01:56:44 +0200

    Add testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,12 @@
 using LiterateDollop, Test
 
-@testset "f" begin
-    x = 5
-    @test f(x) == 6
-end
+ϵ = 1e-5
 
+@testset "optfun" begin
+    f1(x) = x^2
+    f2(x) = (x+1)^4 + 10
+    f3(x) = (x+10)^2 - 10
+    @test optfun(f1, -3, 10) - 0 < ϵ
+    @test optfun(f2, -3, 10) - 1 < ϵ
+    @test optfun(f3, 3, 30) - 10 < ϵ
+end
